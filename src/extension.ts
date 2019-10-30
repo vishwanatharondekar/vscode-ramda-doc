2d1adcdf713dbb848524a4e1956e53d197f42261
Vishwanath Arondekar <vishwanath@dream11.com>
2019-10-30 17:52:20 +0530

    Make use of scheme

--- src/extension.ts
+++ src/extension.ts
@@ -4,26 +4,31 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	vscode.languages.registerHoverProvider('typescript', {
-		provideHover(document, position, token) {
-			const range = document.getWordRangeAtPosition(position);
-			if(range){
-				const newStartPosition = range.start.with(range.start.line, range.start.character - 2)
-				const newEndPosition = range.start.with(range.start.line, range.start.character)
-				const newRange = range.with(newStartPosition, newEndPosition);
-				//TODO : Make this token configurable
-				const tokenToCheck = 'R.';
-				const isRamdaCall = document.getText(newRange) === tokenToCheck;
-				if(isRamdaCall){
-					const functionName = document.getText(range);
-					//TODO : Can we get documentation here itself
-					return {
-						contents : ['https://ramdajs.com/docs/#' + functionName]
-					};
+	//TODO : Make the scope of typescript configurable
+	vscode.languages.registerHoverProvider(
+		{ scheme: 'file', language: 'typescript' }, 
+		{
+
+			provideHover(document, position, token) {
+				const range = document.getWordRangeAtPosition(position);
+				if(range){
+					const newStartPosition = range.start.with(range.start.line, range.start.character - 2)
+					const newEndPosition = range.start.with(range.start.line, range.start.character)
+					const newRange = range.with(newStartPosition, newEndPosition);
+					//TODO : Make this token configurable
+					const tokenToCheck = 'R.';
+					const isRamdaCall = document.getText(newRange) === tokenToCheck;
+					if(isRamdaCall){
+						const functionName = document.getText(range);
+						//TODO : Can we get documentation here itself
+						return {
+							contents : ['https://ramdajs.com/docs/#' + functionName]
+						};
+					}
 				}
 			}
 		}
-	});
+	);
 }
 
 export function deactivate() {
